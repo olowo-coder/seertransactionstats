cf35a60c5c4277bfadf3243391ba6b9aee22832e
Olowo Lanre <lanregolowo@gmail.com>
2024-09-16 16:09:32 -0500

    setup and configuration

---
+++ src/main/java/com/org/seerbitbanking/SeerbitbankingApplication.java
@@ -0,0 +1,13 @@
+package com.org.seerbitbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeerbitbankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeerbitbankingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/org/seerbitbanking/SeerbitbankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.seerbitbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeerbitbankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
