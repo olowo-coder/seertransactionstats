64601c7b6fc40fefa06077ffdad4b3e60dcfefd7
Olowo Lanre <lanregolowo@gmail.com>
2024-09-16 16:13:13 -0500

    setup and configuration

--- src/main/java/com/org/seerbitbanking/SeerbitbankingApplication.java
+++ src/main/java/com/org/seerbitbanking/SeerbitbankingApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SeerbitbankingApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SeerbitbankingApplication.class, args);
     }
